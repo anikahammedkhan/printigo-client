995c8cf2040579ca6ea78572aa906206bb6b7398
Md Anik Ahammed Khan <anikkhan1105@gmail.com>
2022-11-08 17:06:24 +0600

    home carousel added

---
+++ src/Pages/Home/Carousel.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import './Carousel.css';
+import logo1 from '../../Assets/img-1.png';
+import logo2 from '../../Assets/img-2.png';
+import logo3 from '../../Assets/img-3.png';
+
+const Carousel = () => {
+    return (
+        <div className="carousel w-full h-[700px]">
+            <div id="slide1" className="carousel-item relative w-full">
+                <div className='carousel-img'>
+                    <img src={logo1} alt="" className="w-full" />
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
+                    <h1 className='text-6xl font-bold text-white'>
+                        Affordable <br />
+                        Price for Car <br />
+                        Servicing
+                    </h1>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 w-2/5 left-24 top-1/2">
+                    <p className='text-xl text-white'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                </div>
+                <div className="absolute flex justify-start transform -translate-y-1/2 w-2/5 left-24 top-3/4">
+                    <button className="btn btn-warning mr-5">Warning</button>
+                    <button className="btn btn-outline btn-warning">Warning</button>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide3" className="btn btn-circle mr-4">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+                <div className='carousel-img'>
+                    <img alt='' src={logo2} className="w-full" />
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
+                    <h1 className='text-6xl font-bold text-white'>
+                        Affordable <br />
+                        Price for Car <br />
+                        Servicing
+                    </h1>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 w-2/5 left-24 top-1/2">
+                    <p className='text-xl text-white'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                </div>
+                <div className="absolute flex justify-start transform -translate-y-1/2 w-2/5 left-24 top-3/4">
+                    <button className="btn btn-warning mr-5">Warning</button>
+                    <button className="btn btn-outline btn-warning">Warning</button>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide1" className="btn btn-circle mr-4">❮</a>
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <div className='carousel-img'>
+                    <img src={logo3} alt="" className="w-full" />
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
+                    <h1 className='text-6xl font-bold text-white'>
+                        Affordable <br />
+                        Price for Car <br />
+                        Servicing
+                    </h1>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 w-2/5 left-24 top-1/2">
+                    <p className='text-xl text-white'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                </div>
+                <div className="absolute flex justify-start transform -translate-y-1/2 w-2/5 left-24 top-3/4">
+                    <button className="btn btn-warning mr-5">Warning</button>
+                    <button className="btn btn-outline btn-warning">Warning</button>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide2" className="btn btn-circle mr-4">❮</a>
+                    <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Carousel;
---
+++ src/Pages/Shared/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../Assets/logo.png'
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100 py-4 shadow-lg rounded-xl sticky top-0">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/services">Services</Link></li>
+                        <li><Link to="/blog">Blog</Link></li>
+                        <li><Link to="/my-reviews">My Reviews</Link></li>
+                        <li><Link to="/add-service">Add Service</Link></li>
+                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/">Logout</Link></li>
+                    </ul>
+                </div>
+                <Link to="/">
+                    <img src={logo} alt="logo" className="w-1/2" />
+                </Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0 font-semibold text-lg">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/services">Services</Link></li>
+                    <li><Link to="/blog">Blog</Link></li>
+                    <li><Link to="/my-reviews">My Reviews</Link></li>
+                    <li><Link to="/add-service">Add Service</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/">Logout</Link></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <Link className="btn" to="/register">Register</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
