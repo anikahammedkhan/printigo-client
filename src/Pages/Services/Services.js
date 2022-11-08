fce59a7c80fab7d2ce75ed7f17398b17b9faf8e5
Md Anik Ahammed Khan <anikkhan1105@gmail.com>
2022-11-09 03:26:36 +0600

    footer added

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import ServiceSectionCard from '../Home/ServiceSectionCard';
 
 const Services = () => {
